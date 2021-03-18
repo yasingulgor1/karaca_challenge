53f6e0470cf71240ad2b2913f9cc4b0aeed794cf
yasin <yasin_gulgor@hotmail.com>
2021-03-18 15:19:25 +0300

    YasinGulgor(18.03.2021)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable,fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'lazy-loading-images-demo';
+
+  constructor() {
+
+  }
+
+  ngOnInit() { }
+
+
+  defaultImage = "../assets/img/51090.jpg";
+
+  imgArray: string[] = ["../assets/img/karaca1.PNG", "../assets/img/Karaca2.PNG"];
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LazyLoadImageModule} from 'ng-lazyload-image';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LazyLoadImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
